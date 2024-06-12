76e3763e79ce0836d6614a23a98c8167b2655f08
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-06-12 17:53:48 +0200

    generated dashboard and user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -8,7 +8,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatToolbarModule, MatSidenavModule],
+  imports: [RouterOutlet, RouterLink, MatIconModule, MatToolbarModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
