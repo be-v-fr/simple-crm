871357a6f2567997361f546b9e2aa103ab75e4d2
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-06-13 10:41:01 +0200

    added add user dialog form fields

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,19 +1,31 @@
 import { Component, Inject } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import {
-  MatDialog,
   MAT_DIALOG_DATA,
   MatDialogRef,
-  MatDialogTitle,
   MatDialogContent,
   MatDialogActions,
   MatDialogClose,
 } from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatDialogClose],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
